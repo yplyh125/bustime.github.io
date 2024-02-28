bd86416f095584d8d14b4fc6842fef6b9dd4850d
yplyh125 <145650124+yplyh125@users.noreply.github.com>
2024-02-28 14:45:06 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <navheader />
+    <router-view />
+  </div>
+  
+</template>
+<script>
+import navheader from '../src/nav/nav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    navheader
+  }
+}
+</script>
+
+
+<style>
+@import url(//fonts.googleapis.com/earlyaccess/notosanskr.css);
+
+.notosanskr * { 
+ font-family: 'Noto Sans KR', sans-serif;
+}
+
+#app{
+  font-family: 'Noto Sans KR', sans-serif;
+}
+</style>
